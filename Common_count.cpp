2ec97cfe043fecd32a18d0bfa3944d41d45a438c
TejaswiniSingh <161447982+tejapari2005@users.noreply.github.com>
2025-09-09 17:13:03 +0530

    Add files via upload
    This program takes an array of numbers and counts how many times a particular number appears in it. It helps in understanding array traversal, conditional checks, and basic counting logic in programming.

---
+++ Common_count.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() 
+{
+    int array1[50], array2[50];
+    int size1, size2;
+    int Count = 0;
+
+    // Input size of array1 and array2
+    printf("Enter the size of first array: ");
+    scanf("%d", &size1);
+    printf("Enter the size of secound array: ");
+    scanf("%d", &size2);
+    
+    // Input elements of array1
+    printf("\nEnter elements of first array:\n");
+    for (int i = 0; i < size1; i++) 
+	{
+        scanf("%d", &array1[i]);
+    }
+
+    // Input elements of array2
+    printf("\nEnter elements of secound array:\n");
+    for (int i = 0; i < size2; i++) 
+	{
+        scanf("%d", &array2[i]);
+    }
+
+    // Count common elements in both array
+    for (int i = 0; i < size1; i++) 
+	{
+        for (int j = 0; j < size2; j++) 
+		{
+            if (array1[i] == array2[j]) 
+			{
+                Count++;
+                break; 
+            }
+        }
+    }
+
+    // Output the count of common elements
+    printf("\nNumber of common elements in both the array are : %d\n", Count);
+
+    return 0;
+}
+
